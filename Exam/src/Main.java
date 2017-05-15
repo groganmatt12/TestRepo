42e6719f602da7e3cd3e70facfdbcbbe2c2ccf5d
Matt Grogan <Groganmatt95@Matts-Air.home>
2017-05-15 20:21:32 +0100

    Exam, Stage 1

---
+++ Exam/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Person Matt = new Person("Matt", "Grogan", "03/01/1995", "British");
+		System.out.println(Matt.toString());
+	}
+
+}
